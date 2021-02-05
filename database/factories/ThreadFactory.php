ea3bff5a1dbf2431b581b21adaaeec436524e96e
macke89 <mark.opp.1989@gmail.com>
2021-02-05 09:07:52 +0900

    Built Factory Thread, Post

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -2,19 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Models\Post;
+use App\Models\Category;
 use App\Models\Thread;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PostFactory extends Factory
+class ThreadFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Post::class;
+    protected $model = Thread::class;
 
     /**
      * Define the model's default state.
@@ -24,9 +24,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'thread_id' => Thread::all()->random()->id,
-            'user_id' => User::factory(),
-            'body' => $this->faker->text(500)
+            'title' => $this->faker->text(20),
+            'user_id' => User::all()->random()->id,
+            'category_id' => Category::all()->random()->id
         ];
     }
 }
