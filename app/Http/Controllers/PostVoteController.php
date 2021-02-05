851b87e4641a88098f6511d19577232c18b09449
macke89 <mark.opp.1989@gmail.com>
2021-02-05 14:02:28 +0900

    Framework PostVote, Report and Thread Controller

---
+++ app/Http/Controllers/PostVoteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostVote;
+use Illuminate\Http\Request;
+
+class PostVoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PostVote  $postVote
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostVote $postVote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PostVote  $postVote
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostVote $postVote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PostVote  $postVote
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostVote $postVote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PostVote  $postVote
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostVote $postVote)
+    {
+        //
+    }
+}
