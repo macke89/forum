dddcd660b251d2cc03c0ca25401fe0d33b22121e
macke89 <mark.opp.1989@gmail.com>
2021-02-07 23:34:01 +0900

    Setup Index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<x-guest-layout>
+    <header class="text-gray-700 bg-white border-t border-b body-font">
+        <div class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row ">
+            <a class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0">
+                <img src="../badges/WhitePink.svg" alt="">
+            </a>
+            <nav class="flex flex-wrap items-center justify-center text-base ">
+                <a href="#" class="mr-5 text-sm font-semibold text-gray-600 lg:ml-24 hover:text-gray-800">Pricing</a>
+                <a href="#" class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800">Contact</a>
+                <a href="#" class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800">Services</a>
+                <a href="#" class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800">Now</a>
+            </nav>
+            <div class="flex ml-auto">
+                <button
+                    class="inline-flex px-8 py-3 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none">
+                    Button
+                </button>
+                <button
+                    class="items-center px-8 py-3 mt-4 ml-5 font-semibold text-black transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-black0 hover:bg-black hover:text-white focus:ring focus:outline-none">
+                    Button
+                    <svg class="hidden lg:block" fill="none" stroke="currentColor" stroke-linecap="round"
+                         stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </header>
+    <main
+        class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl">
+        <p class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font">
+            -- Content goes here --
+        </p>
+    </main>
+</x-guest-layout>
