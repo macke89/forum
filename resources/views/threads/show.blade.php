aa5c301126b407a8b98e28c84e393b1beb07dbc1
macke89 <42991816+macke89@users.noreply.github.com>
2021-02-27 23:49:53 +0900

    Setup Tests

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -95,7 +95,7 @@
                     @csrf
                     <input type="hidden" id="thread_id" name="thread_id" value="{{ $thread->id }}">
                     <label for="wysiwyg-editor">Answer</label>
-                    <textarea class="form-control w-full" name="postBody" rows="10"></textarea>
+                    <textarea class="form-control w-full" name="postBody" rows="10" required></textarea>
                     <button
                         class="w-full justify-around mt-2 inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-300">
                         Create Post
